114960bc101ac268a559ea2da5c0d201385b4aad
Terrence Ryan <terry@terrenceryan.com>
2012-05-22 17:50:33 -0400

    Getting the site to work as a mobile web application.

--- js/main.js
+++ js/main.js
@@ -2,7 +2,7 @@ jQuery.support.cors = true;
 
 
 function isInPhoneGap(){
- if (typeof PhoneGap === 'undefined') {
+ if (typeof window.device === 'undefined') {
     return false;
   }
   else{
@@ -12,4 +12,9 @@ function isInPhoneGap(){
 
 function getURLParameter(name) {
   return location.toString().split('=')[1];
+}
+
+function isiOS(){
+	var iOS = ( navigator.userAgent.match(/(iPad|iPhone|iPod)/i) ? true : false );
+	return iOS;
 }
